d512ddf30666d27d9d8a1079128b17eab642417a
Mohd Sufiyan <mosufy@gmail.com>
2017-03-17 03:01:12 +0800

    all portfolio loaded via props

--- resources/react/actions/portfolioActions.js
+++ resources/react/actions/portfolioActions.js
@@ -16,6 +16,47 @@ export const resetPortfolio = () => {
           category: "project",
           img: "arena.jpg",
           link: "https://www.thearena.co/"
+        },
+        {
+          title: "Maggie",
+          snippet: "Automates timekeeping for hourly-based professionals, offering their clients credible and accurate invoices.",
+          category: "project",
+          img: "maggie.jpg",
+          link: "https://www.heymaggie.com/"
+        }, {
+          title: "PropertyAPI",
+          snippet: "Provides real estate agents the ability to have their own property listing website integrated via RESTful API.",
+          category: "project",
+          img: "propertyapi.jpg",
+          link: "https://www.propertyapi.com/"
+        },
+        {
+          title: "VixHub",
+          snippet: "Simplifies and packages business processes into a single comprehensive and user-friendly cloud-based software.",
+          category: "app",
+          img: "vixhub.jpg",
+          link: "http://www.vixhub.com/"
+        },
+        {
+          title: "API coLab",
+          snippet: "Tinder for Cheaper Flights concept app for Hack Day. Coded in 6 hours with a 2-min pitch.",
+          category: "experiment",
+          img: "apicolab.jpg",
+          link: "https://github.com/mosufy/apicolab-hackday/"
+        },
+        {
+          title: "BusWhere",
+          snippet: "Search for buses near an area and know when the buses arrive at a specific bus stop using an API.",
+          category: "experiment",
+          img: "buswhere.jpg",
+          link: "https://github.com/mosufy/bus-where"
+        },
+        {
+          title: "PHP MVC Framework",
+          snippet: "Provides basic, barebone essentials to deploy a project quickly. Hosted on GitHub.",
+          category: "experiment",
+          img: "php-mvc.jpg",
+          link: "https://github.com/mosufy/php-mvc"
         }
       ]
     }
