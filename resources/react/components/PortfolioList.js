90fab008e9ba741de6515a477273c7f0ab05bde1
Mohd Sufiyan <mosufy@gmail.com>
2017-03-17 23:00:09 +0800

    Performance improvement to avoid render anonymous function

--- resources/react/components/PortfolioList.js
+++ resources/react/components/PortfolioList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PortfolioItem from './PortfolioItem';
 
+const renderPortfolioItem = (portfolioItem) => {
+  return (
+    <div key={portfolioItem.id}>
+      <PortfolioItem title={portfolioItem.title} snippet={portfolioItem.snippet} category={portfolioItem.category} img={portfolioItem.img} link={portfolioItem.link}/>
+    </div>
+  );
+};
+
 const PortfolioList = ({portfolioItems}) => {
   return (
     <div className="row">
       <div className="portfolio-items">
-        {portfolioItems.map(portfolioItem => {
-          return (
-            <div key={portfolioItem.id}>
-              <PortfolioItem title={portfolioItem.title} snippet={portfolioItem.snippet} category={portfolioItem.category} img={portfolioItem.img} link={portfolioItem.link}/>
-            </div>
-          );
-        })}
+        {portfolioItems.map(renderPortfolioItem)}
       </div>
     </div>
   );
